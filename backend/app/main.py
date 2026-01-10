df0f95968fded6d14d4d3b0c2c8bbb5e07c3d7ea
Yen <a032dsy3371@bangkit.academy>
2026-01-10 13:23:10 +0700

    Setup API

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
