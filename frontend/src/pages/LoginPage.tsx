8417229e05d951005d28b2d4ae833e59c1fdf5cc
Yen <a032dsy3371@bangkit.academy>
2026-01-16 21:25:19 +0700

    Add login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,7 +3,34 @@ type Props = {
 };
 
 function LoginPage({ onLoginSuccess }: Props) {
-  return <div>Login Page</div>;
+  return (
+    <>
+      <div className="login-main">
+        <div className="login-card">
+          <h2 className="login-title">Log In</h2>
+          <p className="login-subtitle">
+            Sign in to manage your tasks and stay productive.
+          </p>
+
+          <form onSubmit={onLoginSuccess} className="vertical-form">
+            <label htmlFor="email-field">Email</label>
+            <input id="email-field" type="email" name="email-field" />
+
+            <label htmlFor="password-field">Password</label>
+            <input id="password-field" type="password" name="password-field" />
+
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </form>
+
+          <p className="login-footer-text">
+            Don’t have an account? <a href="#">Register here!</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
