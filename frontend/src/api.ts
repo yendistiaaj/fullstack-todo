c322520d62a6a0726ec308db1567dd97332555b3
Yen <yendistiaaj@gmail.com>
2026-02-02 15:20:48 +0700

    change base url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://localhost:8000/api',
 });
 
 export default api;
