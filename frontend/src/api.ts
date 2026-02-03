694fe3c0801fbb82dbd18cd4fe84462d5faeb70f
YendistiaAJ <yendistiaaj@gmail.com>
2026-02-03 20:54:07 +0700

    Add token as header

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -5,4 +5,13 @@ const api: AxiosInstance = axios.create({
   baseURL: 'http://localhost:8000/api',
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
