00d26606e4ee0e8f8075bd00d8a908daca2b1749
YendistiaAJ <yendistiaaj@gmail.com>
2026-02-05 16:55:19 +0700

    set backend url to variable

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8000/api',
 });
 
 api.interceptors.request.use((config) => {
